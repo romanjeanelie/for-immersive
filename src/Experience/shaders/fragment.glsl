488fcb9e09d2c928f02cf4acb9f3de84e9b6227a
Roman Jean-Elie <romanjeanelie@yahoo.fr>
2024-01-28 10:07:14 +0100

    feat: improve frag shader

--- src/Experience/shaders/fragment.glsl
+++ src/Experience/shaders/fragment.glsl
@@ -10,39 +10,13 @@ uniform float uOpacity;
 
 varying vec2 vUv;
 
-float PI = 3.1415926535897932384626433832795;
-
-float circle(vec2 uv, vec2 disc_center, float disc_radius, float border_size) {
-	uv -= disc_center;
-	float dist = sqrt(dot(uv, uv));
-	return smoothstep(disc_radius + border_size, disc_radius - border_size, dist);
-}
-
-float aastep(float threshold, float value) {
-    #ifdef GL_OES_standard_derivatives
-	float afwidth = 0.7 * length(vec2(dFdx(value), dFdy(value)));
-	return smoothstep(threshold - afwidth, threshold + afwidth, value);
-    #else
-	return step(threshold, value);
-    #endif
-}
-
 float stroke(float x, float size, float w) {
 	float d = smoothstep(size - 0.1, size + 0.1, x + w * .5) - smoothstep(size - 0.1, size + 0.1, x - w * .5);
 	return clamp(d, 0., 1.);
 }
 
 void main() {
-	vec3 red = vec3(1.0, 0.0, 0.0);
-	vec3 green = vec3(0.0, 1.0, 0.0);
-
-	// Progress y axis
-	// float progress = sin(uTime * 0.0005);
-	float progress = 0.15;
-	float progressY = stroke(vUv.y, 0.5 + (progress * 0.5), uProgress);
-
-	// Line
-	float line = stroke(vUv.x, 0.5, .1);
+	float progressY = stroke(vUv.y, 0.5, uProgress);
 
 	float alpha = progressY * uOpacity;
 
--- src/Experience/shaders/vertex.glsl
+++ src/Experience/shaders/vertex.glsl
@@ -1,42 +1,8 @@
-uniform float uTime;
-uniform float uProgress;
-uniform vec2 uMouse;
-
 varying vec2 vUv;
-
-float PI = 3.1415926535897932384626433832795;
-
-float aastep(float threshold, float value) {
-    #ifdef GL_OES_standard_derivatives
-	float afwidth = 0.7 * length(vec2(dFdx(value), dFdy(value)));
-	return smoothstep(threshold - afwidth, threshold + afwidth, value);
-    #else
-	return step(threshold, value);
-    #endif
-}
-
-// float stroke(float x, float size, float w) {
-// 	float d = aastep(size, x + w * .5) - aastep(size, x - w * .5);
-// 	return clamp(d, 0., 1.);
-// }
-float stroke(float x, float size, float w) {
-	float d = smoothstep(size - 0.1, size + 0.1, x + w * .5) - smoothstep(size - 0.1, size + 0.1, x - w * .5);
-	// step(size, x - w * .5);
-	// aastep(size, x + w * .5) - aastep(size, x - w * .5);
-	return clamp(d, 0., 1.);
-}
 
 void main() {
 	vec3 newposition = position;
 
-    // Zone y
-	float zoneY = stroke(position.y, (0.), .2);
-
-    // Waves
-	float amplitude = 2. * (1. - uProgress);
-	float waves = cos((position.y) * PI * 0.25) * amplitude * zoneY;
-
-	newposition.x += waves;
 	gl_Position = projectionMatrix * modelViewMatrix * vec4(newposition, 1.0);
 
 	vUv = uv;
